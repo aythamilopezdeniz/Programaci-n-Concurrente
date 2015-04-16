42fcfa72a555b978c07f4a75dc8cce99974813f5
Aythami <Aythami@Aythami-VAIO>
2015-04-16 19:26:53 +0100

    Versión 1

--- src/hipermercado/Main.java
+++ src/hipermercado/Main.java
@@ -32,8 +32,6 @@ public class Main {
     }
 
     private static void execute(int Cajas, int Clientes, Cola cola, Contabilidad contabilidad) {
-        long t1 = System.currentTimeMillis();
-        long t2 = System.currentTimeMillis();
         for (int i=0;i<Cajas; i++) {
             cajas.add(new Caja(cola, contabilidad));
         }
@@ -41,20 +39,8 @@ public class Main {
             cajas.get(i).start();
             System.out.println("Abriendo Caja "+(i+1));
         }
-        int tiempoNuevoCliente=(int) (Math.random()*5);
-        esperar(tiempoNuevoCliente);
-        cola.añadirFinal();
         //Esperar un tiempo para añadir nuevo cliente a la cola        
         //esperar hasta los 60s para cerrar la cola.
-        t2 = System.currentTimeMillis();
-        /*while(((t2-t1)/0.001)<3){
-            System.out.println("Espera");
-            if(((t2-t1)/0.001)==2)break;
-        }*/
-        /*if((t2-t1)*0.001==3){
-            System.out.println("hola");
-            //cola.cerrar();
-        }*/
         for(int i=0;i<cajas.size(); i++){
             try {
                 cajas.get(i).join();
@@ -92,13 +78,5 @@ public class Main {
             }
         }
         return clientes;
-    }
-
-    private static void esperar(int tiempoNuevoCliente) {
-        long t1=System.currentTimeMillis();
-        long t2=System.currentTimeMillis();
-        while((t2-t1)*0.001<tiempoNuevoCliente){
-            if((t2-t1)*0.001==tiempoNuevoCliente)break;
-        }
     }
 }
